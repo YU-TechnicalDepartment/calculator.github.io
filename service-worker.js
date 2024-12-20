b7deaac039dbb6647fd5fdafe92545f1771d02af
ガンマ技術部 <yu1000.scratch@gmail.com>
2024-12-20 17:25:18 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.ico',
+        // ここに他の必要なファイルのパスも追加
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
